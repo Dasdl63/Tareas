2fb691f0486915c2fed1525a035868949c256217
Dasdl63 <98928218+Dasdl63@users.noreply.github.com>
2022-02-07 19:51:46 -0600

    Add files via upload

---
+++ TareaActividad2.js
@@ -0,0 +1,28 @@
+"use strict"
+
+
+var mes;
+mes=10;
+mes=prompt("Elija el número de un mes");
+
+if (mes==12 || mes>=1 && mes<=2)
+{
+    console.log("Es Invierno");
+}
+else if (mes>=3 && mes<=5)
+{
+    console.log("Es Primavera")
+}
+else if (mes>=6 && mes<=8)
+ {
+    console.log("Es Verano")
+ }
+ else if (mes>= 9 && mes<=11)
+ {
+    console.log ("Es Otoño")
+ }
+ else
+ {
+    console.log ("Número Inválido, elija uno correcto.")
+ }
+console.log ("Diego Alexander Sum De León, carné# 22000197")
